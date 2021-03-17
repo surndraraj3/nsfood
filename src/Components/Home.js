19216226d4679704554b5ab8318683c5e91891d9
surendranadh123 <surendranadh.t@zoylo.com>
2021-03-17 17:41:42 +0530

    Change plugin

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,67 +1,128 @@
 import React, {Component} from 'react';
-import {Image, Text, View, FlatList, StyleSheet, Dimensions, Button, TouchableHighlight} from 'react-native';
+import {
+  Image,
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView
+} from 'react-native';
+import { Button, Icon } from '@ui-kitten/components';
 import {LoadHomePageData} from '../Services/homepageServices';
-import SearchBar from './searchbar'
+import SearchBar from './searchbar';
+import CONFIG from '../Services/config';
+import LinearGradient from 'react-native-linear-gradient';
 
-const { width } = Dimensions.get("window");
-
-
+const {width} = Dimensions.get('window');
 
 export default class Home extends Component {
   state = {
     productsData: [],
+    count:0
   };
   async componentDidMount() {
     const loadProducts = await new LoadHomePageData();
     this.setState(({productsData}) => ({
       productsData: loadProducts.result.result,
+      count:loadProducts.result.result.length
     }));
   }
   render() {
+    const imageBasePath = CONFIG.productApiUrls.getProductsImagePath;
+    const {productsData, count} = this.state;
     return (
-      <View>
-        <View style={{backgroundColor: "#e23744", padding:10, justifyContent:'center'}}><SearchBar /></View>
-        <FlatList 
-        contentContainerStyle={{ paddingLeft: 5, marginTop: 22 }}
-        data={this.state.productsData}
-        numColumns={1}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item, index }) => {
-          return(
-            <View id={index} style={{ width: width -20, borderWidth:1, borderRadius:10, borderColor:'#d9c7f0',  marginBottom: 20}}>
-            <View style={{flexDirection:'row', padding: 20, justifyContent:'space-between'}}>
-              <View style={{flexDirection:'column'}}>
-              <Image
-                style={{width: 80, height:80}}
-                source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-              />
-              </View>
-              <View style={{flexDirection:'column'}}>
-                <View style={{flexDirection:'row'}}>
-                  <Text style={{fontWeight:'bold', alignSelf:'flex-start', paddingRight:10}}>Product</Text>
-                  <Text style={{alignSelf:'flex-end', alignContent:'flex-end'}}>{item.productName}</Text>
-                </View>
-                <View style={{flexDirection:'row'}}>
-                  <Text style={{fontWeight:'bold', alignSelf:'flex-start', alignContent:'flex-start',flex:0.5,paddingRight:10}}>Price</Text>
-                  <Text style={{alignItems:'flex-end', alignContent:'flex-start', flex:1}}>{'\u20B9'} {item.price}</Text>
-                </View>
-              </View>
-              
+      <View style={styles.wrapper}>
+            <View style={styles.filterWrapper}>
+                <Text style={styles.proNum}>{count} PRODUCTS FOUND</Text>
+                <TouchableOpacity>
+                    {/* <Icon style={{ height: 32, width: 32 }} fill="#000"  /> */}
+                </TouchableOpacity>
             </View>
-            <View style={{flexDirection:'row', paddingLeft: 20, paddingBottom:20}}>
-              <Text style={{flexWrap:'wrap', alignSelf:'flex-start'}}>{'\u2B24'} {item.description}</Text>
-              </View>
-              <View style={{justifyContent:'flex-end', alignContent:'flex-end', alignItems:'flex-end'}}>
-              <TouchableHighlight style={{ width: width /3, height: width / 8, margin:10,alignContent:'center',justifyContent:'center', borderRadius:10, borderWidth:1 }}>
-                <Text style={{alignSelf:'center', alignItems:'center', justifyContent:'center', color:'#000', fontSize:16, fontWeight:'bold'}}>ADD TO CART</Text>
-              </TouchableHighlight>
-              </View>
+            <ScrollView><View style={styles.productWrapper}>
+                {
+                    productsData.map((productsData, index) => {
+                        return <LinearGradient start={{ x: 0, y: 1 }} end={{ x: 0, y: 0 }}
+                            colors={['#FFFFFF', '#D3D3D3']}
+                            style={styles.card} key={`product-list-${index}`}>
+                            <TouchableOpacity>
+                                <Image style={styles.icon} source={{uri: `${imageBasePath}${productsData.icon}`}} />
+                                <Text style={styles.title}>{productsData.title}</Text>
+                                <Text style={styles.desc}>{productsData.productName}</Text>
+                                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', }}>
+                                    <Text style={styles.amount}>{'\u20B9'} {productsData.price}</Text>
+                                    {/* <TouchableOpacity>
+                                        <Icon style={{ height: 25, width: 25 }} fill="#000" />
+                                    </TouchableOpacity> */}
+                                </View>
+                            </TouchableOpacity>
+                        </LinearGradient>
+                    })
+                }
             </View>
-          );
-        }}
-        />
-      </View>
+            </ScrollView>
+        </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 15,
+  },
+  filterWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+  },
+  proNum: {
+    fontSize: 13,
+    color: '#444',
+    fontWeight: 'bold',
+  },
+  productWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+  icon: {
+    height: 120,
+    width: 120,
+    alignSelf: 'center',
+  },
+  card: {
+    elevation: 3,
+    height: 250,
+    //backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 22,
+    width: '47.4%',
+    shadowColor: '#444',
+    shadowOffset: {width: 0, height: 8},
+    shadowOpacity: 0.5,
+    shadowRadius: 8,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    color: '#444',
+  },
+  desc: {
+    fontSize: 13,
+    flexWrap: 'wrap',
+    fontWeight:'bold'
+  },
+  price: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  amount: {
+    fontSize: 18,
+    marginTop: 5,
+  },
+});
--- src/Services/config.js
+++ src/Services/config.js
@@ -3,6 +3,7 @@ const baseUrl = 'http://nsfoodcourt.com/';
 const CONFIG = {
   productApiUrls: {
     getProductsUrl: `${baseUrl}servicesproduct/getProducts`,
+    getProductsImagePath:'https://jagath-dev.s3.ap-south-1.amazonaws.com/Telangana/MyNewsTelugu/'
   },
 };
 
